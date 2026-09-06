2de54db52a3bcbee9eeb438b4d45d98757a8a94e
root <root@5d.localdomain>
2026-09-06 20:47:37 +0800

    NJU-ProjectN/nvboard master initialized without tracing
    NJU-ProjectN/nvboard 4b11dedf1e8a568ad7f74aa68557648b679a9264 README: refine for calling nvboard_update() every cycles

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-STUID = ysyx_22040000
-STUNAME = 张三
+STUID = ysyx_100023197
+STUNAME = 高峰
 
 # DO NOT modify the following code!!!
 
--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,22 @@
-#include <stdio.h>
+#include <Vtop.h>
+#include <verilated.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cassert>
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+int main(int argc, char** argv) {
+    Verilated::commandArgs(argc, argv);
+    Vtop* top = new Vtop;
+
+    while (1) {
+        int a = rand() & 1;
+        int b = rand() & 1;
+        top->a = a;
+        top->b = b;
+        top->eval();
+        printf("a=%d , b=%d , f=%d \n", top->a, top->b, top->f);
+        assert(top->f == (a ^ b));
+    }
+    delete top;
+    return 0;
 }
